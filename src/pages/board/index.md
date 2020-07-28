e494f423a50ed78e2dc12f303e23d54c7540a3f9
David Wolff <info@davidwolff.dk>
2020-07-28 23:17:12 +0200

    wip: board-page is working as intended - needs styling etc

---
+++ src/pages/board/index.md
@@ -0,0 +1,27 @@
+---
+templateKey: board-page
+intro:
+  title: Bestyrelse
+  description: >
+    Foreningen ledes af en bestyrelse på 3-6 medlemmer, som vælges til generalforsamlingen. Bestyrelsens medlemmer vælges for to år ad gangen. Halvdelen af bestyrelsens medlemmer er på valg i lige år, hvor den anden halvdel er på valg i ulige år. Formand og kasserer er ikke på valg i samme år.
+    Genvalg kan finde sted.
+    Udtræder et bestyrelsesmedlem i valgperioden, foretages suppleringsvalg.
+    Bestyrelsen konstituerer sig selv med formand og kasserer. Bestyrelsen er beslutningsdygtig, når mindst to medlemmer deltager i et bestyrelsesmøde.
+    Beslutninger træffes ved simpelt flertal ved håndsoprækning. I tilfælde af stemmelighed, er fomandens stemme afgørende.
+boardmembers:
+  - image: /img/placeholder.png
+    label: Formand
+    name: Hanna Weigelin
+  - image: /img/placeholder.png
+    label: Kasserer
+    name: David Wolff
+  - image: /img/placeholder.png
+    label: Medlem
+    name: Daniel Hildebrandt
+  - image: /img/placeholder.png
+    label: Medlem
+    name: Line Dybvald Askær
+  - image: /img/placeholder.png
+    label: Medlem
+    name: emir Lilic
+---
---
+++ src/templates/board-page.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
+
+export const BoardPageTemplate = ({ title, description, boardmembers }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
+);
+
+BoardPageTemplate.propTypes = {
+  intro: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  boardmembers: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      name: PropTypes.string,
+      image: PropTypes.shape({
+        alt: PropTypes.string,
+        childImageSharp: PropTypes.object,
+        image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+        style: PropTypes.object,
+      }),
+    })
+  ),
+};
+
+const BoardPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+  console.log(frontmatter);
+
+  return (
+    <Layout>
+      <BoardPageTemplate
+        title={frontmatter.intro.title}
+        description={frontmatter.intro.description}
+        boardmembers={frontmatter.boardmembers}
+      />
+    </Layout>
+  );
+};
+
+BoardPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
+
+export default BoardPage;
+
+export const pageQuery = graphql`
+  query BoardPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "board-page" } }) {
+      frontmatter {
+        intro {
+          title
+          description
+        }
+        boardmembers {
+          label
+          name
+          image {
+            childImageSharp {
+              fluid(maxWidth: 250, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
